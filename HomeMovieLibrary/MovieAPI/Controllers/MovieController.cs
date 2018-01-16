dc30fe7fe43ddab77aadd7c40ea0eab4e9964d49
Шиманов Дмитрий Анатольевич <Dmitriy.Shimanov@main.russianpost.ru>
2018-01-16 16:01:15 +0700

    Добавил контроллер и модель в MovieAPI

--- HomeMovieLibrary/MovieAPI/Controllers/MovieController.cs
+++ HomeMovieLibrary/MovieAPI/Controllers/MovieController.cs
@@ -2,40 +2,42 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace MovieAPI.Controllers
 {
-    [Route("api/[controller]")]
-    public class ValuesController : Controller
+    [Produces("application/json")]
+    [Route("api/movie")]
+    public class MovieController : Controller
     {
-        // GET api/values
+        // GET api/movie
         [HttpGet]
         public IEnumerable<string> Get()
         {
             return new string[] { "value1", "value2" };
         }
 
-        // GET api/values/5
+        // GET api/movie/5
         [HttpGet("{id}")]
         public string Get(int id)
         {
             return "value";
         }
 
-        // POST api/values
+        // POST api/movie
         [HttpPost]
         public void Post([FromBody]string value)
         {
         }
 
-        // PUT api/values/5
+        // PUT api/movie/5
         [HttpPut("{id}")]
         public void Put(int id, [FromBody]string value)
         {
         }
 
-        // DELETE api/values/5
+        // DELETE api/movie/5
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
