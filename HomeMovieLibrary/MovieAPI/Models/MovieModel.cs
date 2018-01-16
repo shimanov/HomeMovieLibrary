0b6896e1e7115b1c7370024685d4fecb2c01c9e8
Шиманов Дмитрий Анатольевич <Dmitriy.Shimanov@main.russianpost.ru>
2018-01-16 16:34:43 +0700

    Добавлены комментарии в класс модели movieapi

--- HomeMovieLibrary/MovieAPI/Models/MovieModel.cs
+++ HomeMovieLibrary/MovieAPI/Models/MovieModel.cs
@@ -5,25 +5,60 @@ namespace MovieAPI.Models
     public class MovieModel
     {
         public int Vote_count { get; set; }
+        /// <summary>
+        /// ID Фильма
+        /// </summary>
         public int Id { get; set; }
+
+        /// <summary>
+        /// Наличие Видео
+        /// </summary>
         public bool Video { get; set; }
+
         public double Vote_average { get; set; }
+        /// <summary>
+        /// Название фильма
+        /// </summary>
         public string Title { get; set; }
         public double Popularity { get; set; }
+
+        /// <summary>
+        /// Путь для постера
+        /// </summary>
         public string Poster_path { get; set; }
         public string Original_language { get; set; }
         public string Original_title { get; set; }
         public List<int> Genre_ids { get; set; }
+
+        /// <summary>
+        /// Фоновое иображение
+        /// </summary>
         public string Backdrop_path { get; set; }
         public bool Adult { get; set; }
+
+        /// <summary>
+        /// Описание фильма
+        /// </summary>
         public string Overview { get; set; }
+
+        /// <summary>
+        /// Мировая дата выхода
+        /// </summary>
         public string Release_date { get; set; }
     }
 
-    public class RootObject
+    public class RootObjectMovie
     {
         public int Page { get; set; }
+
+        /// <summary>
+        /// Количество результатов в выдаче
+        /// </summary>
         public int Total_results { get; set; }
+
+        /// <summary>
+        /// Страниц в выдаче
+        /// </summary>
         public int Total_pages { get; set; }
         public List<MovieModel> Results { get; set; }
     }
