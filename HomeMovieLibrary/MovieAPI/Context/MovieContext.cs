99041a2bc15504aa6e7cd7a02a91e00cbf692ba4
Шиманов Дмитрий Анатольевич <Dmitriy.Shimanov@main.russianpost.ru>
2018-01-17 16:15:58 +0700

    Добавил подключение к БД в MovieAPI

---
+++ HomeMovieLibrary/MovieAPI/Context/MovieContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MovieAPI.Models;
+
+namespace MovieAPI.Context
+{
+    public class MovieContext : DbContext
+    {
+        public DbSet<MovieModel> Movies { get; set; }
+        public MovieContext(DbContextOptions<MovieContext> options)
+            : base(options)
+        {
+
+        }
+    }
+}
